1f06c492a6720a70131b7bf7c230451f1aadfdbd
Amit <amitaec94@gmail.com>
2021-09-20 17:32:46 +0530

    Interview notes

---
+++ src/oops/abstraction/ImplementDemoInterface.java
@@ -0,0 +1,14 @@
+package oops.abstraction;
+
+public class ImplementDemoInterface implements DemoInterface {
+
+    @Override
+    public void DemoMethod() {
+
+    }
+
+    @Override
+    public void DemoMethod2() {
+
+    }
+}
