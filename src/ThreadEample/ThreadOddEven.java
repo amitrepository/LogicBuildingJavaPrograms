b2c4332f809a5a11a047fdb48a626069dfad9c2e
Amit <amitaec94@gmail.com>
2022-01-08 01:15:46 +0530

    single

--- src/ThreadEample/ThreadOddEven.java
+++ src/ThreadEample/ThreadOddEven.java
@@ -1,71 +1,70 @@
 package ThreadEample;
 
-import static sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl.ThreadStateMap.Byte0.runnable;
-
 public class ThreadOddEven {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		PrintOddEven ptr= new PrintOddEven();
-		Thread odd = new Thread(new OddEven(ptr,1));
-		Thread even = new Thread(new OddEven(ptr,2));
-		odd.start();
-		even.start();
+        PrintOddEven ptr = new PrintOddEven();
+        Thread odd = new Thread(new OddEven(ptr, 1));
+        Thread even = new Thread(new OddEven(ptr, 2));
+        odd.start();
+        even.start();
 
-	}
+    }
 }
 
 class OddEven implements Runnable {
-	int num;
-	private PrintOddEven ptr;
+    int num;
+    private PrintOddEven ptr;
 
-	public OddEven(PrintOddEven ptr, int num) {
-		this.num = num;
-		this.ptr = ptr;
-	}
+    public OddEven(PrintOddEven ptr, int num) {
+        this.num = num;
+        this.ptr = ptr;
+    }
 
-	public void run() {
-		while(num<=20){
-			if(num%2==0){
-				try {
-					ptr.printEven(num);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}else{
-				try {
-					ptr.printOdd(num);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			num=num+2;
-		}
+    public void run() {
+        while (num <= 20) {
+            if (num % 2 == 0) {
+                try {
+                    ptr.printEven(num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    ptr.printOdd(num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            num = num + 2;
+        }
 
-	}
+    }
 }
 
-class PrintOddEven{
+class PrintOddEven {
 
-	boolean odd= true;
-	public synchronized void printOdd(int i) throws InterruptedException {
-		if(!odd){
-			wait();
-		}
-		System.out.println("ODD: "+i);
-		odd=false;
-		notify();
+    boolean odd = true;
 
-	}
+    public synchronized void printOdd(int i) throws InterruptedException {
+        if (!odd) {
+            wait();
+        }
+        System.out.println("ODD: " + i);
+        odd = false;
+        notify();
 
-	public synchronized void printEven(int i) throws InterruptedException {
-		if (odd) {
-			wait();
-		}
-		System.out.println("EVEN :" +i);
-		odd =true;
-		notify();
-	}
+    }
+
+    public synchronized void printEven(int i) throws InterruptedException {
+        if (odd) {
+            wait();
+        }
+        System.out.println("EVEN :" + i);
+        odd = true;
+        notify();
+    }
 }
 
 
